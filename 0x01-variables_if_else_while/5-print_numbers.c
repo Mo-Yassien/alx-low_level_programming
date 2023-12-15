9bd69bf8ea48731eed14422f9be48fec40b5788c
Mo-Yassien <mo.yassien55@gmail.com>
2023-12-15 09:25:34 -0800

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print all single digit number
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		printf("%i", digit);
+	}
+	printf("\n");
+	return (0);
+}
