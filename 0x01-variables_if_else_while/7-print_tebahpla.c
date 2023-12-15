0a43eea15e2998d98978c5d6bd0150a2d4fcd03d
Mo-Yassien <mo.yassien55@gmail.com>
2023-12-15 09:41:44 -0800

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print all alphapet letters by reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
