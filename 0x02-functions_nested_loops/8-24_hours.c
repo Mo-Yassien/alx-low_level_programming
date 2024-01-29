d744a0072df6d7d1607db33cffac8d210eb131be
Mo-Yassien <mo.yassien55@gmail.com>
2024-01-28 19:43:41 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * Jack_bauer- prints every minute of the day
+ * jack_bauer - prints every minute of the day
 */
 
-void jack_bauer(void) {
-    for (int hour = 0; hour < 24; hour++) {
-        for (int minute = 0; minute < 60; minute++) {
-            printf("%02d:%02d\n", hour, minute);
-        }
-    }
-}
+void jack_bauer(void)
+{
 
-	return 0;
+	for (int hour = 0; hour < 24; hour++)
+	{
+		for (int minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
+
+
+	return (0);
 }
