9cdf026ba4289e0ae4de173275dceb353953de1d
Mo-Yassien <mo.yassien55@gmail.com>
2024-01-31 13:41:20 -0800

    Remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	/**
+	 * leap year when it can be divided
+	 * by 100 and 400 or by 4 evenly
+	 */
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
@@ -24,14 +28,14 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-			if (month == 2 && day == 60)
-			{
+		if (month == 2 && day == 60)
+		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-			}
-			else
-			{
+		}
+		else
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-			}
+		}
 	}
 }
