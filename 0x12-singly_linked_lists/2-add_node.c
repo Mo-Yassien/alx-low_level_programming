fd8354bf01aec65c17a073c40cb07b518a2b9480
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-01 18:36:33 -0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,34 +9,28 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
-
-	/* Allocate memory for the new node */
 	list_t *new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
 
-		return (NULL); /* Allocation failed, return NULL */
+	if (!head || !new_node)
+		return (NULL);
 
-	/* Duplicate the input string using strdup */
-	new_node->str = strdup(str);
 
-	/* Check if string duplication fails */
-	if (new_node->str == NULL)
+	if (str)
 	{
-		free(new_node); /* Free allocated memory for the node */
-		return (NULL);    /* Return NULL if string duplication fails */
+		new_node->str = strdup(str);
+
+		if (!new_node->str)
+		{
+			free(new_node); /* Free allocated memory for the node */
+			return (NULL);    /* Return NULL if string duplication fails */
+		}
+
+		new_node->len = _strlen(new_node->str);
 	}
 
-	/* Calculate the length of the string */
-	new_node->len = strlen(str);
-
-	/* Update pointers to insert the new node at the beginning of the list */
 	new_node->next = *head;
 	*head = new_node;
 
-	/* Return the address of the new node */
 	return (new_node);
 }
-
