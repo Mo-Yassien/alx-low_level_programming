54a4abe53fe0db3f8131c0d4bdba03baa173daf1
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-01 19:00:57 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * myStartupFun - Function to be executed
+ * before the main function.
+ * Prints "You're beat! and yet, you must allow,
+ *		\nI bore my house upon my back!\n".
+ */
+void myStartupFun(void) __attribute__((constructor));
+
+/**
+ * myStartupFun - Function to be executed before the main function.
+ */
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
