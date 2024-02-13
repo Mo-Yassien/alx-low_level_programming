9ec464d607f4aa34883193e8e7fb246c66468460
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-12 18:39:54 -0800

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,37 +12,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters) {
 	if (filename == NULL) {
-		return 0;
+		return (0);
 	}
 
 	int file_descriptor = open(filename, O_RDONLY);
 
 	if (file_descriptor == -1) {
-		return 0;
+		return (0);
 	}
 
 	char *buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL) {
 		close(file_descriptor);
-		return 0;
+		return (0);
 	}
 
 	ssize_t bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1) {
 		close(file_descriptor);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read) {
 		close(file_descriptor);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	close(file_descriptor);
 	free(buffer);
 
-	return bytes_written;
+	return (bytes_written);
 }
