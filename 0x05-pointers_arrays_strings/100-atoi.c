9be213897688db12116544418fdae15b5986507f
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-14 02:21:00 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,39 +9,39 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int digit;
-    int state = 0;  /* 0: before numbers, 1: within numbers */
+	int sign = 1;
+	int result = 0;
+	int digit;
+	int state = 0;  /* 0: before numbers, 1: within numbers */
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
 
-            /* Check for overflow */
-            if (state == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
-            {
-                if (sign == 1)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
+			/* Check for overflow */
+			if (state == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
+			{
+				if (sign == 1)
+					return INT_MAX;
+				else
+					return INT_MIN;
+			}
 
-            result = result * 10 + digit;
-            state = 1;
-        }
-        else if (state == 1)
-        {
-            /* If non-numeric character is encountered after numbers, break the loop */
-            break;
-        }
+			result = result * 10 + digit;
+			state = 1;
+		}
+		else if (state == 1)
+		{
+			/* If non-numeric character is encountered after numbers, break the loop */
+			break;
+		}
 
-        s++;
-    }
+		s++;
+	}
 
-    return result * sign;
+	return result * sign;
 }
