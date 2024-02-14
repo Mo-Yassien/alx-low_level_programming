52295afeada1bd62f1073e742f24980957c2f447
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-14 00:55:25 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,19 +2,17 @@
 
 /**
  * swap_int - swaps the values of two integers.
- *
  * @a: first int parameter
  * @b: second int parameter
  *
 */
 
+
 void swap_int(int *a, int *b)
 {
 	int simp;
+
 	simp = *a;
-
-
-
 	*a = *b;
 	*b = simp;
 }
