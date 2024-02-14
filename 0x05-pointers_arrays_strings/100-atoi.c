61d28a68d6270b0fb3222a62baad4c756e5fb9b1
Mo-Yassien <mo.yassien55@gmail.com>
2024-02-14 02:11:31 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,27 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
+	int i = 0;
 
 	/* Skip leading spaces */
-	while (*s == ' ')
-		s++;
+	while (s[i] == ' ')
+		i++;
 
 	/* Check for sign */
-	while (*s == '-' || *s == '+')
+	while (s[i] == '-' || s[i] == '+')
 	{
-		if (*s == '-')
+		if (s[i] == '-')
 			sign *= -1;
-		s++;
+		i++;
 	}
 
 	/* Convert the digits to integer */
-	while (*s >= '0' && *s <= '9')
+	while (s[i] >= '0' && s[i] <= '9')
 	{
+		int digit = s[i] - '0';
+
 		/* Check for overflow */
-		if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (*s - '0' > INT_MAX % 10)))
+		if (result > (INT_MAX - digit) / 10)
 		{
 			if (sign == 1)
 				return INT_MAX;
@@ -35,8 +38,8 @@ int _atoi(char *s)
 				return INT_MIN;
 		}
 
-		result = result * 10 + (*s - '0');
-		s++;
+		result = result * 10 + digit;
+		i++;
 	}
 
 	return result * sign;
